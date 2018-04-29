c12f8475fb6d77652279d21a877ebef2537a59c9
Chinmay Chandak <chandakchinmay@gmail.com>
2018-04-30 00:35:49 +0530

    Switching between gun and sword done, also wlaking and attack done
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

---
+++ Assets/Scripts/AnimCont.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class AnimCont : MonoBehaviour
+{
+    public Animator anim;
+    public float speedRotation = 4f;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetBool("isAttacking", true);
+        }
+        else
+        {
+            anim.SetBool("isAttacking", false);
+        }
+
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitdist = 0.0f;
+
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speedRotation * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/ModelSwitcher.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelSwitcher : MonoBehaviour
+{
+    public GameObject modelA;
+    public GameObject modelB;
+
+    private int modelNumber;
+
+    void Start()
+    {
+        modelNumber = 1;
+        modelB.SetActive(false);
+    }
+
+    void ModelSwitch()
+    {
+        if (modelNumber == 1)
+        {
+            modelA.SetActive(false);
+            modelB.transform.position = modelA.transform.position;
+            modelB.transform.rotation = modelA.transform.rotation;
+            modelB.SetActive(true);
+            modelNumber = 2;
+        }
+        else if (modelNumber == 2)
+        {
+            modelB.SetActive(false);
+            modelA.transform.position = modelB.transform.position;
+            modelA.transform.rotation = modelB.transform.rotation;
+            modelA.SetActive(true);
+            modelNumber = 1;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            ModelSwitch();
+        }
+
+    }
+}
